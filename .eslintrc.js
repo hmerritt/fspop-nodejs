57d854373ad5917ee7f6e670b888a9095a68fc9e
Harry Merritt <harry@merritt.es>
2020-05-07 00:24:48 +0100

    chore: remove prettier from eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,9 @@
 module.exports = {
-    "plugins": ["prettier"],
-    "extends": ["eslint:recommended", "plugin:prettier/recommended"],
-    "env": {
-        "node": true
+    extends: ["eslint:recommended"],
+    env: {
+        node: true,
     },
-    "parserOptions": {
-        "ecmaVersion": 8,
+    parserOptions: {
+        ecmaVersion: 8,
     },
-    "rules": {
-        "prettier/prettier": [
-			"error", {
-				"tabWidth": 4,
-				"singleQuote": false
-			}
-		]
-    }
-}
+};
