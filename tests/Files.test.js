f3d810b736addb26e3c778c41c5c3e4116fb74a0
Harry Merritt <harry@merritt.es>
2020-05-15 04:05:27 +0100

    test: extend coverage for Files class
    Test for:
    - correctly identifies files
    - correctly identifies directories
    - correctly uses elastic extension

--- tests/Files.test.js
+++ tests/Files.test.js
@@ -6,6 +6,11 @@ dir.testground = "tests/testground/";
 dir.testYaml = dir.testground + "test.yml";
 dir.testDir = dir.testground + "test_mkdir";
 
+// Clean-up testground
+afterAll(() => {
+    fs.rmdirSync(dir.testDir);
+});
+
 describe("mkdir", () => {
     it("creates individual directory", () => {
         // Create directory
@@ -30,8 +35,48 @@ describe("mkdir", () => {
 
         // Test
         expect(error).toEqual(false);
+    });
+});
 
-        // Delete directory
-        fs.rmdirSync(dir.testDir);
+describe("fs checks", () => {
+    it("identifies files", () => {
+        //  Create false-positive directory
+        files.mkdir(dir.testDir, () => {});
+
+        const isFile = files.isFile(dir.testYaml);
+        const isNotFile = files.isFile(dir.testDir);
+
+        //
+        expect(isFile).toEqual(true);
+        expect(isNotFile).toEqual(false);
+    });
+
+    it("identifies directories", () => {
+        //  Create directory to test
+        files.mkdir(dir.testDir, () => {});
+
+        const isDirectory = files.isDirectory(dir.testDir);
+        const isNotDirectory = files.isDirectory(dir.testYaml);
+
+        //
+        expect(isDirectory).toEqual(true);
+        expect(isNotDirectory).toEqual(false);
+    });
+});
+
+describe("elastic extension", () => {
+    it("does not change perfect path", () => {
+        const path = files.elasticExtention(dir.testYaml);
+        expect(path).toEqual(dir.testYaml);
+    });
+
+    it("adds .yml file extension", () => {
+        const path = files.elasticExtention(`${dir.testground}test`);
+        expect(path).toEqual(dir.testYaml);
+    });
+
+    it("returns empty string if nothing found", () => {
+        const emptyPath = files.elasticExtention(`${dir.testground}idonotexist`);
+        expect(emptyPath).toEqual("");
     });
 });
